d50c5f5cdebf20a3cef1148adf3d55e3ec9a5ac5
ringosan1974 <wacyakun@gmail.com>
2022-03-28 16:54:04 +0900

    fetch()の引数修正

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 import client from 'cheerio-httpcli';
 
 const url = 'https://scraping-for-beginner.herokuapp.com/image';
-const param = {};
 
-client.fetch(url, param, (err, $, res) => {
+client.fetch(url, (err, $, res) => {
   if (err) {
     console.log('error');
     return;
