c7f4ba444c6e80bd22402472756263f325f0f878
ringosan1974 <wacyakun@gmail.com>
2022-04-29 22:03:58 +0900

    保存できない問題を改善

--- download.js
+++ download.js
@@ -1,15 +1,14 @@
 import client from 'cheerio-httpcli';
 import fs from 'fs';
-import { setTimeout } from 'timers/promise';
 
-const url = 'https://scraping-for-beginner.herokuapp.com/image';
-const html_elem = 'img.materialbox.responsive-img.card';
+const url = 'url';
+const html_elem = 'element';
 
 let counter = 0;
 
 client.download
 .on('ready',(stream) => {
-  const write = fs.createWriteStream(`.\\image\\image${counter}.png`);
+  const write = fs.createWriteStream(`./image/image${counter}.png`);
   counter++;
   write
     .on('finish',() => {
@@ -36,6 +35,5 @@ client.download.parallel = 4;
 // スクレイピング開始
 client.fetch(url,(err, $, res, body) => {
   $(html_elem).download();
-  await setTimeout(1000);
   console.log('OK!');
 });
